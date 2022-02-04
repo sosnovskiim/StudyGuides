ff445b823edafd97d9475e0e19a78c33422890b8
sosnovskiim <darkisosn2805@gmail.com>
2022-02-04 23:33:06 +0400

    Initial commit

---
+++ app/src/main/java/com/sosnowskydevelop/studyguides/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sosnowskydevelop.studyguides.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sosnowskydevelop.studyguides.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
